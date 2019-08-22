5ae96a745c30187505e47063daf49fbf06fbcdca
vishal-pandey <contact@vishalpandey.xyz>
2019-08-22 17:54:39 +0530

    First Build

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 var mqtt = require('mqtt')
 
+var client  = mqtt.connect('wss://www.vishalpandey.xyz:8080')
+
+client.on('connect', function () {
+  client.subscribe('iotswitch', function (err) {
+    if (!err) {
+      // client.publish('iotswitch', 'My Cannel Testing 1')
+    }
+  })
+})
+
+
+client.on('message', function (topic, message) {
+	if (window.location.pathname == '/') {
+	  if (message.toString() == '1') {
+	  	console.log("On")
+
+	  	document.querySelector('.off').style = 'display:none';
+	  	document.querySelector('.on').style = 'display:flex';
+	  }else if(message.toString() == '0'){
+	  	console.log("Off")
+	  	document.querySelector('.off').style = 'display:flex';
+	  	document.querySelector('.on').style = 'display:none';
+	  }
+	}
+	if (window.location.pathname == '/control.html') {
+	  if (message.toString() == '1') {
+	  	console.log("On")
+	  	document.querySelector('.button-on').setAttribute('disabled', true);
+			document.querySelector('.button-off').removeAttribute('disabled');
+	  }else if(message.toString() == '0'){
+	  	console.log("Off")
+	  	document.querySelector('.button-on').removeAttribute('disabled');
+			document.querySelector('.button-off').setAttribute('disabled', true);
+	  }
+	}
+})
+
+
 window.onload = ()=>{
-	var btn = document.querySelector('.submit').addEventListener('click', (e)=>{
-		e.preventDefault()
-		var client  = mqtt.connect('wss://www.vishalpandey.xyz:8080')
-
-
-		var mytopic = document.querySelector('input').value
-
-		client.on('connect', function () {
-		  client.subscribe(mytopic, function (err) {
-		    if (!err) {
-		      client.publish(mytopic, 'My Cannel Testing 1')
-		    }
-		  })
+	if (window.location.pathname == '/control.html') {
+		document.querySelector('.button-on').addEventListener('click', ()=>{
+			client.publish('iotswitch', '1')
+			// document.querySelector('.button-on').setAttribute('disabled', true);
+			// document.querySelector('.button-off').removeAttribute('disabled');
 		})
-
-		// client.on('message', function (topic, message) {
-		//   console.log(message.toString())
-		// })
-	})
+		document.querySelector('.button-off').addEventListener('click', ()=>{
+			client.publish('iotswitch', '0')
+			// document.querySelector('.button-on').removeAttribute('disabled');
+			// document.querySelector('.button-off').setAttribute('disabled', true);
+		})
+	}
 }
